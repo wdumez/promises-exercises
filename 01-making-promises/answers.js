2ad0060dfad27d20a2e462f8251ed4ca5a1bbe5c
William Dumez <william.dumez@skynet.be>
2023-11-13 21:18:08 +0100

    Solved 01

--- 01-making-promises/answers.js
+++ 01-making-promises/answers.js
@@ -5,8 +5,8 @@
  * 
  * @returns {Promise<3>}
  */
-function makePromiseResolveWith3(){
-  /* IMPLEMENT ME! */
+function makePromiseResolveWith3() {
+  return Promise.resolve(3);
 }
 
 /**
@@ -15,8 +15,8 @@ function makePromiseResolveWith3(){
  * 
  * @returns {Promise<,"Boo!">}
  */
-function makePromiseRejectWithBoo(){
-  /* IMPLEMENT ME! */
+function makePromiseRejectWithBoo() {
+  return Promise.reject("Boo!")
 }
 
 /**
@@ -27,11 +27,13 @@ function makePromiseRejectWithBoo(){
  * @returns {Promise<undefined, undefined>}
  */
 
-function makePromiseWithConstructor(itShouldResolve){
+function makePromiseWithConstructor(itShouldResolve) {
   return new Promise((resolve, reject) => {
-    /* IMPLEMENT ME! */
-    /* If itShouldResolve is true, call resolve */
-    /* If itShouldResolve is false, call reject */
+    if (itShouldResolve) {
+      resolve();
+    } else {
+      reject();
+    }
   });
 }
 
@@ -43,9 +45,12 @@ function makePromiseWithConstructor(itShouldResolve){
  * @param {number} delayInMs 
  * @return {Promise<any>} - A promise that will resolve with the value after delayInMs milliseconds
  */
-function makeDelayPromise(value, delayInMs){
-  /* IMPLEMENT ME! */
-  /* Return a promise that resolves with the value after delayInMs */
+function makeDelayPromise(value, delayInMs) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(value);
+    }, delayInMs);
+  });
 }
 
 module.exports = {
