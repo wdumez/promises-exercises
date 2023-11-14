9c5b1e6d3ddc1d92f7a636f7ea09ad4dc3d2cad5
William Dumez <william.dumez@skynet.be>
2023-11-14 15:27:54 +0100

    Solved 05

--- 05-callbacks/answers.js
+++ 05-callbacks/answers.js
@@ -3,9 +3,9 @@
  * @param {string} password 
  * @returns {User | undefined}
  */
-function passwordChecker(email, password){
-  if(email === 'jeff@jeff.jeff' && password === 'jeff'){
-    return {name: 'Jeff Jeffries', email: 'jeff@jeff.jeff'};
+function passwordChecker(email, password) {
+  if (email === 'jeff@jeff.jeff' && password === 'jeff') {
+    return { name: 'Jeff Jeffries', email: 'jeff@jeff.jeff' };
   }
   return undefined;
 }
@@ -15,9 +15,9 @@ function passwordChecker(email, password){
  * @param {string} password 
  * @param {nodeStyleCallback} cb 
  */
-function passwordCheckerCb(email, password, cb){
+function passwordCheckerCb(email, password, cb) {
   const user = passwordChecker(email, password);
-  if(user){
+  if (user) {
     cb(null, user);
   } else {
     setTimeout(() => {
@@ -34,10 +34,15 @@ function passwordCheckerCb(email, password, cb){
  * @param {string} password 
  * @returns {Promise<User, string>}
  */
-function passwordCheckerPrms(email, password){
+function passwordCheckerPrms(email, password) {
   return new Promise((resolve, reject) => {
     passwordCheckerCb(email, password, (error, user) => {
-      /* IMPLEMENT ME! */
+      if (error) {
+        reject(error)
+      }
+      else {
+        resolve(user)
+      }
     });
   });
 }
